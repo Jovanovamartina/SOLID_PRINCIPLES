8323fabbdcdfe11e80f363f3b26321be054418ff
Jovanovamartina <98462497+Jovanovamartina@users.noreply.github.com>
2024-02-17 00:55:38 +0100

    change

---
+++ SolidPrinciples/SolidPrinciples/DependencyInversion/SalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿
+namespace SolidPrinciples.DependencyInversion
+{
+    public class SalaryCalculator
+    {
+        public float CalculateSalary(int hoursWorked, float hourlyRate) => hoursWorked * hourlyRate;
+    }
+    public class EmployeeDetails
+    {
+        public int HoursWorked { get; set; }
+        public int HourlyRate { get; set; }
+        public float GetSalary()
+        {
+            var salaryCalculator = new SalaryCalculator();
+            return salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+        }
+    }
+}
