42015c46981e014b0597f7d2a11bfe297854c306
Jovanovamartina <98462497+Jovanovamartina@users.noreply.github.com>
2024-02-17 00:44:53 +0100

    change

---
+++ SolidPrinciples/SolidPrinciples/LiskovSubstitution/AppleSolid.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SolidPrinciples.LiskovSubstitution
+{
+    public interface IFruit
+    {
+        string GetColor();
+    }
+    public class AppleSolid : IFruit
+    {
+        public string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class OrangeSolid : IFruit
+    {
+        public string GetColor()
+        {
+            return "Orange";
+        }
+    }
+}
+
