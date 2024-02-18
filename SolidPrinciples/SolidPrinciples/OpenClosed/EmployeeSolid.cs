be6f407ccd6cf376daf96e41a836c737b0887db3
Jovanovamartina <98462497+Jovanovamartina@users.noreply.github.com>
2024-02-18 17:38:27 +0100

    change

--- SolidPrinciples/SolidPrinciples/OpenClosed/EmployeeSolid.cs
+++ SolidPrinciples/SolidPrinciples/OpenClosed/EmployeeSolid.cs
@@ -6,13 +6,7 @@ namespace SolidPrinciples.OpenClosed
         double CalculateSalary();
     }
 
-    public class SalaryCalculatorSolid
-    {
-        public double CalculateSalary(IEmployee employee)
-        {
-            return employee.CalculateSalary();
-        }
-    }
+  
 
     public class FullTimeEmployee : IEmployee
     {
@@ -46,6 +40,14 @@ namespace SolidPrinciples.OpenClosed
         }
     }
 
-  
+    public class SalaryCalculatorSolid
+    {
+        public double CalculateSalary(IEmployee employee)
+        {
+            return employee.CalculateSalary();
+        }
+    }
+
+
 }
 
